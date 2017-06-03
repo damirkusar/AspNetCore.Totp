796da8ff67fc06ef729e8db900c511b9780bb9b8
Damir Kusar <damir@kusar.ch>
2017-06-04 00:40:25 +0200

    Added test.

--- AspNetCore.Totp.Tests/TotpValidatorTests.cs
+++ AspNetCore.Totp.Tests/TotpValidatorTests.cs
@@ -5,15 +5,26 @@ namespace AspNetCore.Totp.Tests
     public class TotpValidatorTests
     {
         private readonly TotpValidator totpValidator;
+        private readonly TotpGenerator totpGenerator;
+        
         public TotpValidatorTests()
         {
             this.totpValidator = new TotpValidator();
+            this.totpGenerator = new TotpGenerator();
+        }
+
+        [Fact]
+        public void Validate_TotpGeneratedByGoogleAuthenticatorIsNotValid()
+        {
+            var validated = this.totpValidator.Validate("7FF3F52B-2BE1-41DF-80DE-04D32171F8A3", 284621);
+            Assert.False(validated);
         }
 
         [Fact]
         public void Validate_TotpGeneratedByGoogleAuthenticatorIsValid()
         {
-            var validated = this.totpValidator.Validate("7FF3F52B-2BE1-41DF-80DE-04D32171F8A3", 284628);
+            var totp = totpGenerator.Generate("7FF3F52B-2BE1-41DF-80DE-04D32171F8A3");
+            var validated = this.totpValidator.Validate("7FF3F52B-2BE1-41DF-80DE-04D32171F8A3", totp);
             Assert.True(validated);
         }
     }
