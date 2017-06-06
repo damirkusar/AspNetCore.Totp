a57b9b51cafbb8eee57d3a9bc38cc3aad0a02ab6
Damir Kusar <damir.kusar@axpo.com>
2017-06-06 08:00:19 +0200

    Fixed warnings.

--- AspNetCore.Totp.Tests/TotpValidatorTests.cs
+++ AspNetCore.Totp.Tests/TotpValidatorTests.cs
@@ -23,7 +23,7 @@ namespace AspNetCore.Totp.Tests
         [Fact]
         public void Validate_TotpGeneratedByGoogleAuthenticatorIsValid()
         {
-            var totp = totpGenerator.Generate("7FF3F52B-2BE1-41DF-80DE-04D32171F8A3");
+            var totp = this.totpGenerator.Generate("7FF3F52B-2BE1-41DF-80DE-04D32171F8A3");
             var validated = this.totpValidator.Validate("7FF3F52B-2BE1-41DF-80DE-04D32171F8A3", totp);
             Assert.True(validated);
         }
