eb325f37c5cae815a97c4f72637421ccd8c59c13
Damir Kusar <damirkusar@axpo.com>
2018-05-31 10:16:33 +0200

    Updated.

--- AspNetCore.Totp/Properties/AssemblyInfo.cs
+++ AspNetCore.Totp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Avectris AG")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AspNetCore.Totp")]
 [assembly: AssemblyTrademark("")]
 
